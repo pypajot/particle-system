7b7d94e747b6f42c3f0bf37265d9c9c503f38a2f
Pierre-yves PAJOT <ppajot@f6r4s1.clusters.42paris.fr>
2025-09-05 17:58:48 +0200

    camera comments

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -4,25 +4,38 @@
 #include "math/vec3.hpp"
 #include "math/mat4.hpp"
 
+/// @brief The class dealing with the movement of the camera but also the projection matrix and coordinate change from screen to world
 class Camera
 {
     private:
+        /// @brief The current projection matrix
         mat4 _proj;
         
+        /// @brief The camera rotation speed in radians
         const float _rotateSpeed = 0.01f;
+        /// @brief The camera movement speed in world coordinates
         const float _moveSpeed = 0.01f;
         
+        /// @brief The camera posistion
         vec3 _position;
+        /// @brief The camera direction
         vec3 _direction;
     
     public:
+        /// @brief The fov of the camera
         const float fov = toRadians(60.0f);
+        /// @brief the distance to the near clipping plane
         const float near = 0.1f;
+        /// @brief the distance to the far clipping plane
         const float far = 100.0f;
 
+        /// @brief Integer to represent the front / back movement of the camera. A positive value means the camera is moving forward, a negative one backward
         int moveFrontBack;
+        /// @brief Integer to represent the left / right movement of the camera. A positive value means the camera is moving left, a negative one right
         int moveLeftRight;
+        /// @brief Integer to represent the up / down movement of the camera. A positive value means the camera is moving up, a negative one down
         int moveUpDown;
+        /// @brief Integer to represent the left / right rotation of the camera. A positive value means the camera is rotating left, a negative one right
         int rotateLeftRight;
 
         Camera();
@@ -37,6 +50,4 @@ class Camera
         
         void computeProjectionMatrix(float height, float width);
         mat4 coordToScreenMatrix() const;
-
-
 };
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 
+/// @brief Construct a Camera object at default position (0, 0, 2) and direction (0, 0, 1)
 Camera::Camera()
 {
     _position = vec3(0, 0, 2);
@@ -17,6 +18,8 @@ Camera::Camera()
     rotateLeftRight = 0;
 }
 
+/// @brief Constrcut a Camera object with default direction (0, 0, 1)
+/// @param pos The position at which the camera will be set on creation 
 Camera::Camera(vec3 pos)
 {
     _position = pos;
@@ -58,6 +61,7 @@ Camera Camera::operator=(const Camera &other)
     return *this;
 }
 
+/// @brief Move the cameras depending on its position and direction
 void Camera::move()
 {
     _direction.y += _rotateSpeed * rotateLeftRight;
@@ -65,24 +69,32 @@ void Camera::move()
     mat4 rotate(1.0f);
     
     if (movement.length() == 0)
-    return;
+        return;
+
     movement *= 1 / movement.length();
     rotate = rotation(rotate, -_direction.y, vec3(0.0f, 1.0f, 0.0f));
     movement = rotate * vec4(movement, 1.0f);
     _position += movement * _moveSpeed;
 }
 
+/// @brief Reset teh camera position and direction
 void Camera::resetPosition()
 {
     _position = vec3(0, 0, 2);
     _direction = vec3(0, 0, 1);
 }
 
+/// @brief Computes the projection matrix
+/// @note Need to be called when the window changes size
+/// @param height The height of the window
+/// @param width The width
 void Camera::computeProjectionMatrix(float height, float width)
 {
     _proj = perspective(fov, width / height, near, far);
 }
 
+/// @brief Computes the matrix to convert world coordinates to screen coordinate sfor use by openGL
+/// @return The ematrix
 mat4 Camera::coordToScreenMatrix() const
 {
     vec3 cameraFront(-std::sin(_direction.y), 0.0f, -std::cos(_direction.y));
